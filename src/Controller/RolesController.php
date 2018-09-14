84b48e4bea0df4db4ff741e77626f80d7e240015
michaeldeloris <michaeldeldev@gmail.com>
2018-09-14 15:00:49 +0200

    Adding method createRole in RolesController

--- src/Controller/RolesController.php
+++ src/Controller/RolesController.php
@@ -8,20 +8,65 @@ Use App\Model\Role;
 class RolesController extends Controller
 {
 
-    function showRoles()
+    public function showRoles()
     {
         $sql = 'SELECT * FROM app_roles';
         $sth = $this->getPDO()->prepare($sql);
         $sth->execute();
-        $roles = $sth->fetchAll(\PDO::FETCH_CLASS, Role::class);
         return $this->render('role/roles.html.php', [
-            "roles" => $roles
+            "roles" => $sth->fetchAll(\PDO::FETCH_CLASS, Role::class),
+            "token" => $this->getToken()
         ]);
     }
 
-    function deleteRole()
+    public function deleteRole($id)
     {
-        exit;
+        if (!$this->isCsrfTokenValid()) {
+            throw new \UnexpectedValueException("Invalid token");
+        }
+
+        $sql = "DELETE FROM `app_roles` WHERE `app_roles`.`id` =:id ";
+        $sth = $this->getPDO()->prepare($sql);
+        $sth->bindValue(":id", (int)$id, \PDO::PARAM_INT);
+        $sth->execute();
+
+        $response = $this->getResponse();
+        $response->setHeader([
+            "Location" => "./../../roles"
+        ]);
+        return $response;
+    }
+
+    public function createRole()
+    {
+        if (!$this->isCsrfTokenValid()) {
+            throw new \UnexpectedValueException("Invalid token");
+        }
+        $name = filter_input(INPUT_POST, "name", FILTER_VALIDATE_REGEXP,[
+            "options" => [
+                "regexp" => Role::NAME
+            ]
+        ]);
+
+        $value = filter_input(INPUT_POST, "value", FILTER_VALIDATE_REGEXP,[
+                "options" => [
+                    "regexp" => Role::VALUE
+                ]
+            ]);
+
+        if($name && $value){
+           $sql = "INSERT INTO `app_roles`(`name`, `value`) VALUES (:name, :value)";
+            $sth = $this->getPDO()->prepare($sql);
+            $sth->bindValue(":name", $name, \PDO::PARAM_INT);
+            $sth->bindValue(":value", $value, \PDO::PARAM_INT);
+            $sth->execute();
+        }
+
+        $response = $this->getResponse();
+        $response->setHeader([
+            "Location" => "./../../public/roles"
+        ]);
+        return $response;
 
     }
 
